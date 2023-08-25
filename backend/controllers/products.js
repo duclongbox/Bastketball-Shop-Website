1e7919ba395cd4c56f5f715e3426366529bf1330
Haolin Ma <127691823+haolinma1@users.noreply.github.com>
2023-08-25 17:28:34 -0400

    display the shoes based on page

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,11 +1,18 @@
 const shoes=require('../models/schema')
 
-
+// get all the nike shoe based on the page
 const getAllNikeproducts=async (req,res)=>{
+   const {page}=req.query
+   // limit each page contain 10 shoes 
+   const pagePerItem=10
+   const skipItems=(page-1)*pagePerItem
    const nikeShoes=await shoes.find({brand:'nike'})
+   .skip(skipItems)
+   .limit(pagePerItem)
    res.status(200).json({nikeShoes})
 }
 
+// get the specific nike shoe by the 
 const getProductsByName=async (req,res)=>{
     
      res.json(`The id is ${req.params.id}`)
