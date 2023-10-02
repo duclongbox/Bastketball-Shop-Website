fac8049f854878c37db44a2529b2d174f3e49582
Haolin Ma <127691823+haolinma1@users.noreply.github.com>
2023-10-02 10:18:06 -0400

    Finish Paginate

--- client/src/components/NikeComponent.jsx
+++ client/src/components/NikeComponent.jsx
@@ -1,6 +1,28 @@
 import { useEffect, useState } from "react";
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
+
+function Items({ currentItems }) {
+  return (
+    <div className="grid lg:grid-cols-3 gap-4 pl-44 pt-10">
+      {currentItems &&
+        currentItems.map((item) => (
+          <div key={item._id} className="p-10">
+            <Link to="dwd">
+              <img
+                src={item.imageURL}
+                alt="dww"
+                className="h-40 w-auto max-w-30"
+              />
+              <p>Name: {item.name}</p>
+              <p>Brand: {item.brand}</p>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+}
 
 const NikeComponent = () => {
   // initialize the current state, fetch data from express
@@ -17,24 +39,44 @@ const NikeComponent = () => {
       }
     };
 
-    fetchData(); // Call the fetch function inside useEffect
-
-    // Specify an empty dependency array to run the effect only once after the initial render
+    fetchData();
   }, []);
- 
+  // paginate
+  const itemsPerPage = 6;
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = data.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className="grid lg:grid-cols-3 gap-4 pl-44 pt-10">
-      {data.map((item, index) => (
-
-        <div key={item._id} className="p-10">
-        <Link to='dwd' >
-        <img src={item.imageURL} alt="dww" className="h-40 w-auto max-w-30" />
-          <p>Name: {item.name}</p>
-          <p>Brand: {item.brand}</p>
-          </Link>
-        </div>
-      ))}
-
+    <div>
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        className="flex  gap-4 justify-center p-4"
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel=""
+        renderOnZeroPageCount={null}
+      ></ReactPaginate>
     </div>
   );
 };
